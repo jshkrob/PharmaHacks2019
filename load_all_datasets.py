f638b2afcd87b28122ed196a8a699073560ea264
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-11-09 22:26:50 -0500

    added verbosity-functionality, finished docs, added `load_all_datasets` alias

--- load_all_datasets.py
+++ load_all_datasets.py
@@ -1,13 +1,16 @@
 # standard library dependencies 
+from typing import Mapping
 import os
 
 # external dependencies
 import pandas as pd 
 
-def compile_datasets(data_dir:str=None, must_have_substring:str="cleaned_"):
+def load_all_datasets(data_dir:str=None, must_have_substring:str="cleaned_",
+                    verbose:bool=False) -> Mapping[str, pd.DataFrame]:
     """
-    Loads and saves all the text files containing
-    'cleaned_', but not ".zip".
+    Loads and the contents of every dataset (tab-separated .txt files) as a 
+    pandas DataFrame and saves them into a dictionary of (filename:pd.DataFrame)
+    (key:value) pairs.
     ----------
     Parameters:
         data_dir:   optional string path to the data directory.
@@ -15,6 +18,34 @@ def compile_datasets(data_dir:str=None, must_have_substring:str="cleaned_"):
                     directory.
         must_have_substring:    optional string that all files to load must contain.
                                 defaults to "cleaned_"
+        verbose:    boolean indicator of verbosity.
+                    defaults to False.
+    ----------
+    Returns:
+        dataset_data:   dictionary of (filename: pandas.DataFrame) (key:value) 
+                        pairs.
+    ----------
+    Doctest:
+    >>> datasets_dictionary = load_all_datasets(r"data") # indicates that the data folder is a subfolder of the current folder
+    >>> datasets_dictionary = load_all_datasets(r"path/to/the/data/folder")
+    """
+
+def compile_datasets(data_dir:str=None, must_have_substring:str="cleaned_",
+                    verbose:bool=False) -> Mapping[str,pd.DataFrame]:
+    """
+    Depreciated alias to the load_all_datasets function.
+    Loads and the contents of every dataset (tab-separated .txt files) as a 
+    pandas DataFrame and saves them into a dictionary of (filename:pd.DataFrame)
+    (key:value) pairs.
+    ----------
+    Parameters:
+        data_dir:   optional string path to the data directory.
+                    defaults to None, which gets replaced by the current 
+                    directory.
+        must_have_substring:    optional string that all files to load must contain.
+                                defaults to "cleaned_"
+        verbose:    boolean indicator of verbosity.
+                    defaults to False.
     ----------
     Returns:
         dataset_data:   dictionary of (filename: pandas.DataFrame) (key:value) 
@@ -24,31 +55,5 @@ def compile_datasets(data_dir:str=None, must_have_substring:str="cleaned_"):
     >>> datasets_dictionary = compile_datasets(r"data") # indicates that the data folder is a subfolder of the current folder
     >>> datasets_dictionary = compile_datasets(r"path/to/the/data/folder")
     """
-    # if no data_dir argument is given, 
-    # we assume we need to use the current directory
-    # (the directory which this program is saved in)
-    if data_dir is None: 
-        data_dir = os.getcwd()
-
-    dataset_data = {}
-    for diritem in os.listdir(data_dir):
-        # don't look into folders
-        if not os.path.isfile(os.path.join(data_dir, diritem)): continue
-        
-        # don't look into zipped files
-        if ".zip" in diritem: continue
-        
-        # only consider files that have "cleaned_" and ".txt" in their name
-        elif must_have_substring in diritem and ".txt" in diritem:
-
-            # load the contents of the file
-            df = pd.read_csv(
-                os.path.join(data_dir, diritem),
-                index_col=0,
-                header=0,
-                sep='\t'
-            )
-            print(f"\n{diritem}: {df.shape}")
-            dataset_data[diritem] = df
-    print(f"\nfinished compiling {len(dataset_data)} datasets")
-    return dataset_data
+    return load_all_datasets(data_dir=data_dir, must_have_substring=must_have_substring,
+                            verbose=verbose)
