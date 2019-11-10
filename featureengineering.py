c31a87d38c2ffd953a4f4c5e68248b9996850bcb
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-11-10 11:24:00 -0500

    added a fix for unintentional < 0 values in engineered features

--- featureengineering.py
+++ featureengineering.py
@@ -14,8 +14,9 @@ def correct_shift(datasets):
 def add_features(list_of_dfs):
     for index, df in enumerate(list_of_dfs):
         df["Enrollment Time"] = df["Day Enrollment Completed"] - df["Day Enrollment Received"]
+        df[ df["Enrollment Time"] < 0 ] = 0
         df["On Drug Time"] = df[["Last On Drug Day", "On Drug Start Day"]].apply(correct_shift, axis=1)
-        df["# of Payment Methods"] = df[["Payment Method #4", "Payment Method #5", "Payment Method #1", "Payment Method #3", "Payment Method #2"]].sum(axis=1)
+        df["# of Payment Methods"] = max(0, df[["Payment Method #4", "Payment Method #5", "Payment Method #1", "Payment Method #3", "Payment Method #2"]].sum(axis=1) )
         list_of_dfs[index] = df
         
     return list_of_dfs
