075651b442d8821bd878f9ba6433a7f1d1d7b8c4
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-11-10 11:28:23 -0500

    repeat of last commit's changes, but with the `add_engineered_features` function

--- featureengineering.py
+++ featureengineering.py
@@ -37,6 +37,7 @@ def testingdosagefrequencyindex():
 def add_engineered_features(df):
     with_new_features = df.copy(deep=True)
     with_new_features["Enrollment Time"] = with_new_features["Day Enrollment Completed"] - with_new_features["Day Enrollment Received"]
+    with_new_features[ with_new_features["Enrollment Time"] < 0 ] = 0
     with_new_features["On Drug Time"] = with_new_features[["Last On Drug Day", "On Drug Start Day"]].apply(correct_shift, axis=1)
     with_new_features["# of Payment Methods"] = with_new_features[["Payment Method #4", "Payment Method #5", "Payment Method #1", "Payment Method #3", "Payment Method #2"]].sum(axis=1)
     with_new_features["DosageFrequencyIndex"] = 3*with_new_features["Dosage"] + with_new_features["Frequency"]
