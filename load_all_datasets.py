8da2b1aa9accafb693413ef1c693fc1c82cbad05
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-11-10 11:09:02 -0500

    re-wrote code that was accidentally deleted

--- load_all_datasets.py
+++ load_all_datasets.py
@@ -29,6 +29,34 @@ def load_all_datasets(data_dir:str=None, must_have_substring:str="cleaned_",
     >>> datasets_dictionary = load_all_datasets(r"data") # indicates that the data folder is a subfolder of the current folder
     >>> datasets_dictionary = load_all_datasets(r"path/to/the/data/folder")
     """
+    # if no data_dir argument is given, 
+    # we assume we need to use the current directory
+    # (the directory which this program is saved in)
+    if data_dir is None: 
+        data_dir = os.getcwd()
+
+    dataset_data = {}
+    for diritem in os.listdir(data_dir):
+        # don't look into folders
+        if not os.path.isfile(os.path.join(data_dir, diritem)): continue
+        
+        # don't look into zipped files
+        if ".zip" in diritem: continue
+        
+        # only consider files that have "cleaned_" and ".txt" in their name
+        elif must_have_substring in diritem and ".txt" in diritem:
+
+            # load the contents of the file
+            df = pd.read_csv(
+                os.path.join(data_dir, diritem),
+                index_col=0,
+                header=0,
+                sep='\t'
+            )
+            if verbose: print(f"\n{diritem}: {df.shape}")
+            dataset_data[diritem] = df
+    if verbose: print(f"\nfinished compiling {len(dataset_data)} datasets")
+    return dataset_data
 
 def compile_datasets(data_dir:str=None, must_have_substring:str="cleaned_",
                     verbose:bool=False) -> Mapping[str,pd.DataFrame]:
