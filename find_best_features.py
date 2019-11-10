295de5e73e77d8408d77d721f8f1bc73bb757463
samy.coulombe <samy.coulombe@mail.mcgill.ca>
2019-11-09 22:37:00 -0500

    wrapped up documentation

--- find_best_features.py
+++ find_best_features.py
@@ -3,13 +3,14 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
-def find_best_features(X, y):
+def find_best_features(X:np.ndarray, y:np.ndarray) -> np.ndarray:
     '''
-        X - numpy array 
-        y - numpy array of response variable
-        
+    Parameters:
+        X: numpy array 
+        y: numpy array of response variable
+    Returns:
+        The numpy array returned by sklearn.feature_selection.SelectFromModel
     '''
-    
     # Selector obj using Random Forest classifier
     rf = RandomForestClassifier(n_estimators=1000, max_depth=5, random_state=0)
     sfm = SelectFromModel(rf, threshold=0.15)
